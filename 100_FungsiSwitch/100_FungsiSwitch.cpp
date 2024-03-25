3effeb57be3c98ab22d7d42310d99bead447fff3
riqsans <thrq88@gmail.com>
2024-03-25 14:20:57 +0700

    Add project files.

---
+++ 100_FungsiSwitch/100_FungsiSwitch.cpp
@@ -0,0 +1,9 @@
+// 100_FungsiSwitch.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int bilSatu, bilDua;
+
+
