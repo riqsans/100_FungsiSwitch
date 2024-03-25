2f3df396604f5d44949f0ef4786d5fab2156b758
riqsans <thrq88@gmail.com>
2024-03-25 15:04:01 +0700

    membenarkan fungsi while

--- 100_FungsiSwitch/100_FungsiSwitch.cpp
+++ 100_FungsiSwitch/100_FungsiSwitch.cpp
@@ -45,30 +45,34 @@ int main()
         cout << "5. Exit" << endl;
         cout << "Masukan pilihan 1/2/3/4/5/6 = ";
         cin >> pilihan;
-    }
-}
 
-switch (pilihan)
-{
-case 1:
-    input();
-    cout << "Hasil Penjumlahan = " << penjumlahan(bilSatu, bilDua) << endl;
-    break;
-case 2:
-    input();
-    cout << "Hasil Pembagian = " << pembagian(bilSatu, bilDua) << endl;
-    break;
-case 3:
-    input();
-    cout << "Hasil Perkalian = " << perkalian(bilSatu, bilDua) << endl;
-    break;
-case 4:
-    input();
-    cout << "Hasil Pengurangan = " << pengurangan(bilSatu, bilDua) << endl;
-    break;
-case 5:
-    break;
-default:
-    cout << "Pilihan tidak ada" << endl;
-    break;
-}
+
+
+        switch (pilihan)
+        {
+        case 1:
+            input();
+            cout << "Hasil Penjumlahan = " << penjumlahan(bilSatu, bilDua) << endl;
+            break;
+        case 2:
+            input();
+            cout << "Hasil Pembagian = " << pembagian(bilSatu, bilDua) << endl;
+            break;
+        case 3:
+            input();
+            cout << "Hasil Perkalian = " << perkalian(bilSatu, bilDua) << endl;
+            break;
+        case 4:
+            input();
+            cout << "Hasil Pengurangan = " << pengurangan(bilSatu, bilDua) << endl;
+            break;
+        case 5:
+            break;
+        default:
+            cout << "Pilihan tidak ada" << endl;
+            break;
+
+        }while (pilihan != 5);
+
+        return 0;
+    }
