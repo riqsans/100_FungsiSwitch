66736f2fe941e4f89e1fd9a20adab96da9f06871
riqsans <thrq88@gmail.com>
2024-03-25 14:47:13 +0700

    Switch pilihan

--- 100_FungsiSwitch/100_FungsiSwitch.cpp
+++ 100_FungsiSwitch/100_FungsiSwitch.cpp
@@ -47,3 +47,28 @@ int main()
         cin >> pilihan;
     }
 }
+
+switch (pilihan)
+{
+case 1:
+    input();
+    cout << "Hasil Penjumlahan = " << penjumlahan(bilSatu, bilDua) << endl;
+    break;
+case 2:
+    input();
+    cout << "Hasil Pembagian = " << pembagian(bilSatu, bilDua) << endl;
+    break;
+case 3:
+    input();
+    cout << "Hasil Perkalian = " << perkalian(bilSatu, bilDua) << endl;
+    break;
+case 4:
+    input();
+    cout << "Hasil Pengurangan = " << pengurangan(bilSatu, bilDua) << endl;
+    break;
+case 5:
+    break;
+default:
+    cout << "Pilihan tidak ada" << endl;
+    break;
+}
